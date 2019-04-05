7c9c7cfcce913e8f9ec50601c15a536e8c9339ba
Andrew Maurice <andromulin@gmail.com>
2019-04-05 15:32:08 -0500

    attempt at creating binary string
    simple build based on notes to test the server timing for getting the covert binary string

--- chat-covert.py
+++ chat-covert.py
@@ -13,18 +13,38 @@
 #              covert message in stdout as well.
 ################################################################################
 
-
-
+from time import time
 import socket
 import sys
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = 'jeangourd.com'
-port = 31337
+ip = '138.47.136.187'
+port = 5550
+
+#hold the binary for the covert message
+covertBin = ""
+#the times that output a 1 or 0 for the covert message, respectively
+TIME_ONE = 1
+TIME_ZERO = 0.25
+
 
 s.connect((ip, port))
 
-data = s.recv(4096)
+#data = s.recv(4096)
 while (data.rstrip("\n") != "EOF"):
+        start = time()
+        data = s.recv(4096)
+        end = time()
+
+        delta = round(end-start, 3)
+        if(delta >= TIME_ONE):
+                covertBin += "1"
+        else:
+                covertBin += "0"
+        
 	sys.stdout.write(data)
 	sys.stdout.flush()
-s.close
+s.close()
+
+print(covertBin)
+
+
